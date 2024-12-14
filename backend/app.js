25600eb2f37add7966cb9c25d026931adcd14f37
Rishi096 <rishirajtiwari200002@gmail.com>
2024-12-14 17:32:47 +0530

    Added full-stack project with frontend and backend

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
+const path = require('path');
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require("cors");
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,20 +18,27 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
+
+// Serve React static build
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors());
+// Backend routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// Catch-all route to serve React frontend for unmatched routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
